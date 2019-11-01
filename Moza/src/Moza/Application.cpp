06b7a3cb236c10c75d064e3a5b5d77294e9f32f8
Lyon77 <rjl7799@gmail.com>
2019-11-01 01:52:53 -0400

    meaningless commit

--- Moza/src/Moza/Application.cpp
+++ Moza/src/Moza/Application.cpp
@@ -17,7 +17,7 @@ namespace Moza
 	{
 		MZ_CORE_ASSERT(!s_Instance, "Application already exists!");
 		s_Instance = this;
-
+		MZ_CORE_INFO("ran");
 		m_Window = std::unique_ptr<Window>(Window::Create());
 		m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 	}
